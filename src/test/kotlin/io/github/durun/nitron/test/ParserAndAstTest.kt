0d780594295426387825f1d32267ab54a7619e19
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-10-08 16:21:28 +0900

    add parsing test

---
+++ src/test/kotlin/io/github/durun/nitron/test/ParserAndAstTest.kt
@@ -0,0 +1,87 @@
+package io.github.durun.nitron.test
+
+import io.github.durun.nitron.parser.CommonParser
+import io.github.durun.nitron.tester.ParserTester
+import io.github.durun.nitron.tester.reserializeJson
+import io.kotlintest.data.forall
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.kotlintest.tables.row
+import org.snt.inmemantlr.exceptions.CompilationErrorException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.text.ParseException
+import java.util.stream.Collectors
+
+
+val baseDir = Paths.get("testdata/grammars").toAbsolutePath()
+
+class ParserAndAstTest: StringSpec({
+    "parse calculator" {
+        val grammarFiles = listOf(baseDir.resolve("calculator/calculator.g4"))
+        val exampleFiles = listOf(baseDir.resolve("calculator/examples/number1.txt"))
+        val startRule = "equation"
+        val asts = ParserTester(grammarFiles, startRule, exampleFiles).getAsts()
+        asts.forEach {
+            val jsons = reserializeJson(it)
+            jsons.first shouldBe jsons.second
+        }
+    }
+
+    "parse and json serialize" {
+        forall(
+                row("kotlin", ".kt", "kotlinFile"),
+                row("java", ".java", "compilationUnit"),
+                row("java8", ".java", "compilationUnit"),
+                row("java9", ".java", "compilationUnit"),
+                row("c", ".c", "compilationUnit"),
+                row("python3", ".py", "file_input"),
+                row("ruby", ".rb", "prog"),
+                row("scala", ".txt", "compilationUnit"),
+                row("swift3", ".swift", "top_level"),
+                row("typescript", ".ts", "program"),
+                row("javascript", ".js", "program"),
+                row("golang", ".go", "sourceFile"),
+                row("csharp", ".cs", "compilation_unit"),
+                row("cpp", ".cpp", "translationunit"),
+                row("objc", ".m", "translationUnit")
+
+        ) { langDir, suffix, startRule ->
+            val grammarDirDepth = when(langDir){
+                "objc" -> 2
+                else -> 1
+            }
+
+            val grammarDir = baseDir.resolve(langDir)
+            val exampleDir = grammarDir.resolve("examples")
+            val grammarFiles = Files.walk(grammarDir, grammarDirDepth)
+                    .filter { it.toFile().isFile }
+                    .filter { it.toString().endsWith(".g4")}
+                    .collect(Collectors.toList())
+
+            val exampleFiles = Files.walk(exampleDir, 3)
+                    .filter { it.toFile().isFile }
+                    .filter { it.endsWith(suffix) }
+                    .collect(Collectors.toList())
+
+            val parserTester = {
+                val result = kotlin.runCatching {
+                    ParserTester(grammarFiles, startRule, exampleFiles)
+                }
+                result.getOrNull() ?: throw CompilationErrorException("failed to compile ${langDir}")
+            }()
+
+            val asts = {
+                val result = kotlin.runCatching {
+                    parserTester.getAsts()
+                }
+                result.getOrNull() ?: throw Exception("failed to parse in ${langDir}")
+            }()
+
+            asts.forEach {
+                val jsons = reserializeJson(it)
+                jsons.first shouldBe jsons.second
+            }
+        }
+    }
+})
---
+++ src/test/kotlin/io/github/durun/nitron/tester/ParserTester.kt
@@ -0,0 +1,24 @@
+package io.github.durun.nitron.tester
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.github.durun.nitron.ast.basic.AstBuildVisitor
+import io.github.durun.nitron.ast.basic.AstNode
+import io.github.durun.nitron.parser.CommonParser
+import java.nio.file.Path
+
+class ParserTester(
+        val grammarFiles: List<Path>,
+        val startRuleName: String,
+        val inputFiles: List<Path>
+) {
+    private val parser = CommonParser(grammarFiles)
+    fun getAsts(): List<AstNode> {
+        val ast = inputFiles.mapNotNull {
+            val result = parser.parse(it, startRuleName)
+            val tree = result.first
+            val parser = result.second
+            tree.accept<AstNode?>(AstBuildVisitor(parser))
+        }
+        return ast
+    }
+}
