b4330ad9996a5d774cc8855a6e013038234fbcd8
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-10-08 16:29:57 +0900

    po

---
+++ src/test/kotlin/io/github/durun/nitron/GPSandbox.kt
@@ -0,0 +1,39 @@
+package io.github.durun.nitron
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.github.durun.nitron.ast.basic.AstBuildVisitor
+import org.snt.inmemantlr.GenericParser
+import org.snt.inmemantlr.listener.DefaultListener
+import java.nio.file.Paths
+
+fun main() {
+    val dir = Paths.get("testdata/grammars/kotlin")
+    val gParser = GenericParser(
+            dir.resolve("KotlinLexer.g4").toFile(),
+            dir.resolve("KotlinParser.g4").toFile(),
+            dir.resolve("UnicodeClasses.g4").toFile()
+
+    )
+    val input = """fun main() { println("hello") }"""//dir.resolve("examples/Test.kt").toFile()
+    val production = "kotlinFile"
+
+
+    gParser.compile()
+    println("compiled.")
+    val listener = ParserListener()
+    gParser.setListener(listener)
+
+    val result = gParser.parse(input, production, GenericParser.CaseSensitiveType.NONE)
+    val parser = listener.getParser() ?: throw Exception("can't get parser before parse")
+    val ast = result.accept(AstBuildVisitor(parser))
+
+    val mapper = jacksonObjectMapper()
+    val json = mapper.writeValueAsString(ast)
+    println(json)
+
+}
+
+
+class ParserListener(): DefaultListener(){
+    fun getParser() = this.parser ?: null
+}
