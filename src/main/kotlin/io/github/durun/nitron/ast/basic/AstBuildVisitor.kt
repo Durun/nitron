d0163eb11b1058f3bc56eadbe97b2e279591e55f
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-10-08 16:17:14 +0900

    add AstBuildVisitor

---
+++ src/main/kotlin/io/github/durun/nitron/ast/basic/AstBuildVisitor.kt
@@ -0,0 +1,43 @@
+package io.github.durun.nitron.ast.basic
+
+import io.github.durun.nitron.antlr4util.children
+import org.antlr.v4.runtime.Parser
+import org.antlr.v4.runtime.tree.*
+
+class AstBuildVisitor(
+        private val parser: Parser
+) : ParseTreeVisitor<AstNode> {
+
+    override fun visitChildren(node: RuleNode?): AstNode {
+        val children = node?.children?.map { it.accept(this) }
+                ?: throw Exception("RuleNode has no children.")
+
+        val ruleIndex = node.ruleContext?.ruleIndex
+                ?: throw Exception("Rulenode has no ruleIndex")
+        val ruleName = parser.ruleNames[ruleIndex]
+                ?: throw Exception("can't get ruleName")
+
+        return AstRuleNode(
+                ruleName = ruleName,
+                children = children
+        )
+    }
+
+    override fun visitTerminal(node: TerminalNode?): AstNode {
+        val token = node?.text
+                ?: throw Exception("TerminalNode has no text")
+        return AstTerminalNode(
+                token = token
+        )
+    }
+
+
+    override fun visitErrorNode(node: ErrorNode?): AstNode {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun visit(tree: ParseTree?): AstNode {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
