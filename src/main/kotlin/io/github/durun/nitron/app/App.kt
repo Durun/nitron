70b6703df61d96f4189bfa8a09512e8ed16b669b
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-10-09 18:56:40 +0900

    add command help

--- src/main/kotlin/io/github/durun/nitron/app/App.kt
+++ src/main/kotlin/io/github/durun/nitron/app/App.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.path
 import io.github.durun.nitron.ast.basic.AstBuildVisitor
 import io.github.durun.nitron.getGrammarList
@@ -12,12 +13,24 @@ import java.io.PrintStream
 import java.nio.file.Path
 
 class App: CliktCommand() {
-    val grammarDir: Path by argument().path()
-    val inputPath: Path by argument().path()
-    val outputPath: Path? by option().path()
-    val startRule: String? by option()
+    private val grammarDir: Path by argument(
+            name = "grammar_dir",
+            help = "directory containing grammar(.g4) files"
+    ).path()
+    private val inputPath: Path by argument(
+            name = "input",
+            help = "input file to parse"
+    ).path()
+    private val outputPath: Path? by option(
+            names = *arrayOf("--output", "-o"),
+            help = "output file"
+    ).path()
+    private val startRule: String by option(
+            names = *arrayOf("--start", "-s"),
+            help = "the start rule(defined in grammar file)"
+    ).required()
 
-    val output = PrintStream(
+    private val output = PrintStream(
             outputPath?.toFile()?.outputStream()
                     ?: System.out
     )
