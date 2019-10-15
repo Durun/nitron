4ad3e410163fbba78b33fb83eab1e43734ff3c25
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-10-15 14:47:23 +0900

    change: using NormalizingRuleMap
    複数の文法規則の親子の組を正規化に対応付けることを可能にした

--- src/main/kotlin/io/github/durun/nitron/ast/normalizing/NormalizePrintVisitor.kt
+++ src/main/kotlin/io/github/durun/nitron/ast/normalizing/NormalizePrintVisitor.kt
@@ -5,36 +5,45 @@ import io.github.durun.nitron.ast.AstVisitor
 import io.github.durun.nitron.ast.basic.AstTerminalNode
 import io.github.durun.nitron.ast.basic.AstRuleNode
 import java.lang.IllegalStateException
+import java.util.*
+import kotlin.collections.HashMap
 
 class NormalizePrintVisitor(
-        val nonNumberedRuleMap: Map<String, String>,
-        val numberedRuleMap: Map<String, String>
+        val nonNumberedRuleMap: NormalizingRuleMap,
+        val numberedRuleMap: NormalizingRuleMap
 ): AstVisitor<String> {
     // Map: (normalizedRuleName -> (id -> count))
     private val nameTables: Map<String, MutableMap<String, Int>> = numberedRuleMap.values.associateWith { HashMap<String, Int>() }
+
+    private val visitedRuleStack: Stack<String> = Stack()
 
     override fun visit(node: AstNode): String {
         TODO()
     }
 
     override fun visitRule(node: AstRuleNode): String {
+        visitedRuleStack.push(node.ruleName)    //  enter
         val rule = node.ruleName
         val id = node.getText() ?: ""
         val thisText = normalizeRuleIfNeeded(rule, id)
         val childrenText = node.children?.joinToString(" ") { it.accept(this) }
+        visitedRuleStack.pop()                  // leave
         return thisText ?: childrenText ?: ""
     }
 
     override fun visitTerminal(node: AstTerminalNode): String {
-        return normalizeTokenIfNeeded(node.tokenType) ?: node.token
+        visitedRuleStack.push(node.tokenType)   //  enter
+        val thisText = normalizeTokenIfNeeded(node.tokenType) ?: node.token
+        visitedRuleStack.pop()                  // leave
+        return thisText
     }
 
     private fun normalizeTokenIfNeeded(tokenType: String): String? {
-        return nonNumberedRuleMap[tokenType]
+        return nonNumberedRuleMap[visitedRuleStack]
     }
     private fun normalizeRuleIfNeeded(rule: String, id: String): String? {
-        return nonNumberedRuleMap[rule]
-                ?: numberedRuleMap[rule]?.let{ "${it}${getAndUpdateRuleCount(it, id)}" }
+        return nonNumberedRuleMap[visitedRuleStack]
+                ?: numberedRuleMap[visitedRuleStack]?.let{ "${it}${getAndUpdateRuleCount(it, id)}" }
     }
     private fun getAndUpdateRuleCount(normalizedRuleName: String, id: String): Int {
         val idTable = nameTables[normalizedRuleName] ?: throw IllegalStateException("No such rule in nameTables")
