0386976ab3ca8dea10fff76b0da646c8f3147cd1
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-10-21 12:35:24 +0900

    add ChangesWriter, CodesWriter class
    writer for tables

---
+++ src/main/kotlin/io/github/durun/nitron/data/model/table/writer/ChangesWriter.kt
@@ -0,0 +1,7 @@
+package io.github.durun.nitron.data.model.table.writer
+
+import io.github.durun.nitron.data.model.Change
+import io.github.durun.nitron.data.model.table.Changes
+import org.jetbrains.exposed.sql.Database
+
+class ChangesWriter(db: Database): TableWriter<Change> by BufferedTableWriter(db, Changes, idColumn = Changes.id)
---
+++ src/main/kotlin/io/github/durun/nitron/data/model/table/writer/CodesWriter.kt
@@ -0,0 +1,7 @@
+package io.github.durun.nitron.data.model.table.writer
+
+import io.github.durun.nitron.data.model.Code
+import io.github.durun.nitron.data.model.table.Codes
+import org.jetbrains.exposed.sql.Database
+
+class CodesWriter(db: Database): TableWriter<Code> by BufferedTableWriter(db, Codes, idColumn = Codes.id)
