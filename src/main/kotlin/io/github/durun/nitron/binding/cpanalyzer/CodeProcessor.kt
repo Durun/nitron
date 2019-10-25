e6a9e8fb280fb1c9d4798d3fe337cb8705c0a024
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-10-25 16:31:23 +0900

    add CodeProcessor, StatementProvider class

---
+++ src/main/kotlin/io/github/durun/nitron/binding/cpanalyzer/CodeProcessor.kt
@@ -0,0 +1,40 @@
+package io.github.durun.nitron.binding.cpanalyzer
+
+import io.github.durun.nitron.core.ast.AstNode
+import io.github.durun.nitron.core.ast.basic.AstBuildVisitor
+import io.github.durun.nitron.core.ast.normalizing.NormalizePrintVisitor
+import io.github.durun.nitron.core.ast.normalizing.NormalizingRuleMap
+import io.github.durun.nitron.core.config.LangConfigLoader
+import io.github.durun.nitron.core.parser.CommonParser
+import java.nio.file.Path
+
+class CodeProcessor(configFile: Path) {
+    private val parser: CommonParser
+    private val startRule: String
+    private val splitRules: List<String>
+    private val nonNumberedRuleMap: NormalizingRuleMap
+    private val numberedRuleMap: NormalizingRuleMap
+
+    init {
+        val config = LangConfigLoader.load(configFile)
+        val baseDir = configFile.parent
+        parser = CommonParser(
+                grammarFiles = config.grammarConfig.grammarFilePaths(baseDir),
+                utilityJavaFiles = config.grammarConfig.utilJavaFilesPaths(baseDir)
+        )
+        startRule = config.grammarConfig.startRule
+        splitRules = config.processConfig.splitConfig.splitRules
+        nonNumberedRuleMap = config.processConfig.normalizeConfig.nonNumberedRuleMap
+        numberedRuleMap = config.processConfig.normalizeConfig.numberedRuleMap
+    }
+
+    fun process(input: String): List<Pair<AstNode, String>> {
+        val (tree, antlrParser) = parser.parse(input, startRule)
+        val ast = tree.accept(AstBuildVisitor(antlrParser))
+        val statements = ast.pickRecursiveByRules(splitRules)
+        return statements.map {
+            val normalizeVisitor = NormalizePrintVisitor(nonNumberedRuleMap, numberedRuleMap)
+            Pair(it, it.accept(normalizeVisitor))
+        }
+    }
+}
