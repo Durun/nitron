d3bde70f66f25b7c4d6869552637376e19c9093c
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-10-25 16:31:54 +0900

    add @JvmField annotation to Statement class

--- src/main/kotlin/io/github/durun/nitron/binding/cpanalyzer/Statement.kt
+++ src/main/kotlin/io/github/durun/nitron/binding/cpanalyzer/Statement.kt
@@ -4,12 +4,18 @@ import java.security.MessageDigest
 
 
 class Statement(
+        @JvmField
         val tokens: List<Token>,
+        @JvmField
         val rText: String,
+        @JvmField
         val nText: String
 ) {
+    @JvmField
     val fromLine: Int = tokens.first().line
+    @JvmField
     val toLine: Int = tokens.last().line
+    @JvmField
     val hash: ByteArray = digester.digest(nText.toByteArray())
 
     companion object {
@@ -18,7 +24,10 @@ class Statement(
 }
 
 data class Token(
+        @JvmField
         val value: String,
+        @JvmField
         val line: Int,
+        @JvmField
         val index: Int
 )
