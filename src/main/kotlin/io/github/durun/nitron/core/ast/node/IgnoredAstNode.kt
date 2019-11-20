e2f5c22cc28e23e2820aadba6a551d46685d8bc0
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-11-20 16:18:54 +0900

    remove class IgnoredAstNode

--- src/main/kotlin/io/github/durun/nitron/core/ast/node/IgnoredAstNode.kt
+++
@@ -1,7 +0,0 @@
-package io.github.durun.nitron.core.ast.node
-
-class IgnoredAstNode : AstNode {
-    override val range: TextRange? = null
-    override val children: List<AstNode>? = null
-    override fun getText(): String? = ""
-}
