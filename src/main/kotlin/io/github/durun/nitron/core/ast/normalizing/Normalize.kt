cc3797c8e93d0425e64d08c7156dcb9b4f0950c6
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-11-20 15:35:41 +0900

    delete outdated file

--- src/main/kotlin/io/github/durun/nitron/core/ast/normalizing/Normalize.kt
+++
@@ -1,5 +0,0 @@
-package io.github.durun.nitron.core.ast.normalizing
-
-import io.github.durun.nitron.core.ast.basic.BasicAstRuleNode
-
-fun BasicAstRuleNode.normalize(): NormalAstRuleNode = NormalAstRuleNode(this)
