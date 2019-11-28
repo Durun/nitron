86eadb2e38068a75d88c8dcdd4abcc097ca0d8b1
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-11-28 18:56:53 +0900

    add SerializableAstBuilder

---
+++ src/main/kotlin/io/github/durun/nitron/inout/model/ast/SerializableAstBuilder.kt
@@ -0,0 +1,49 @@
+package io.github.durun.nitron.inout.model.ast
+
+import io.github.durun.nitron.core.ast.node.AstNode
+import io.github.durun.nitron.core.ast.node.AstRuleNode
+import io.github.durun.nitron.core.ast.node.AstTerminalNode
+import io.github.durun.nitron.core.ast.node.NormalAstRuleNode
+import io.github.durun.nitron.core.ast.visitor.AstVisitor
+import io.github.durun.nitron.core.toHash
+
+fun AstNode.toSerializable(nodeTypeSet: NodeTypeSet): Structure {
+    return Structure(
+            nodeTypeSet = nodeTypeSet,
+            ast = this.accept(SerializableAstBuildVisitor(nodeTypeSet)),
+            hash = this.toHash()
+    )
+}
+
+private class SerializableAstBuildVisitor(
+        private val typeMap: NodeTypeSet
+) : AstVisitor<SerializableAst.Node> {
+    override fun visit(node: AstNode): SerializableAst.Node {
+        return SerializableAst.TerminalNode(   // TODO
+                type = -1,
+                text = ""
+        )
+    }
+
+    override fun visitRule(node: AstRuleNode): SerializableAst.Node {
+        val type = typeMap.rule(node.ruleName) ?: throw NoSuchElementException()
+        return if (node is NormalAstRuleNode) {
+            SerializableAst.NormalizedRuleNode(
+                    type,
+                    text = node.getText()
+            )
+        } else {
+            SerializableAst.RuleNode(
+                    type,
+                    children = node.children?.map { it.accept(this) }.orEmpty()
+            )
+        }
+    }
+
+    override fun visitTerminal(node: AstTerminalNode): SerializableAst.Node {
+        return SerializableAst.TerminalNode(
+                type = typeMap.token(node.tokenType) ?: throw NoSuchElementException(),
+                text = node.token
+        )
+    }
+}
