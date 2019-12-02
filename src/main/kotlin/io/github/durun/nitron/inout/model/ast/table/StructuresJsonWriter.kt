867fee1b355774bd14b9d45556fd5e07a5f6ef18
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-12-02 19:23:29 +0900

    change: StructuresJsonWriter flushes automatically

--- src/main/kotlin/io/github/durun/nitron/inout/model/ast/table/StructuresJsonWriter.kt
+++ src/main/kotlin/io/github/durun/nitron/inout/model/ast/table/StructuresJsonWriter.kt
@@ -1,19 +1,20 @@
 package io.github.durun.nitron.inout.model.ast.table
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import io.github.durun.nitron.core.decodeByteArray
 import io.github.durun.nitron.core.encodeByteArray
 import io.github.durun.nitron.inout.model.ast.NodeTypeSet
 import io.github.durun.nitron.inout.model.ast.Structure
 import io.github.durun.nitron.inout.model.table.writer.TableWriter
 import java.io.Closeable
 import java.io.File
+import java.io.Flushable
 import java.io.PrintWriter
 
 class StructuresJsonWriter(
         private val out: PrintWriter,
-        nodeTypeSet: NodeTypeSet
-) : TableWriter<Structure>, Closeable {
+        nodeTypeSet: NodeTypeSet,
+        private val autoFlush: Boolean = true
+) : TableWriter<Structure>, Flushable, Closeable {
     companion object {
         private val mapper = jacksonObjectMapper()
     }
@@ -28,30 +29,46 @@ class StructuresJsonWriter(
         write(nodeTypeSet)
     }
 
-    override fun close() {
+    override fun flush() {
         out.flush()
+    }
+
+    override fun close() {
+        flush()
         out.close()
     }
 
     private fun write(value: NodeTypeSet) {
         val json = mapper.writeValueAsString(value)
         out.println(json)
+        if (autoFlush) flush()
+    }
+
+    private fun writeAsString(value: Structure): String {
+        val hash = encodeByteArray(value.hash)
+        val ast = mapper.writeValueAsString(value.ast)
+        return "{$hash:$ast}"
     }
 
     override fun write(value: Structure) {
-        val hash = encodeByteArray(value.hash)
-        val p = decodeByteArray(hash)
-        assert(p.contentEquals(value.hash))
-        val ast = mapper.writeValueAsString(value.ast)
-        val json = "{$hash:$ast}"
+        val json = writeAsString(value)
         out.println(json)
+        if (autoFlush) flush()
     }
 
     override fun write(values: List<Structure>) {
-        values.forEach { write(it) }
+        values.forEach {
+            val json = writeAsString(it)
+            out.println(json)
+        }
+        if (autoFlush) flush()
     }
 
     override fun write(values: Sequence<Structure>) {
-        values.forEach { write(it) }
+        values.forEach {
+            val json = writeAsString(it)
+            out.println(json)
+        }
+        if (autoFlush) flush()
     }
 }
