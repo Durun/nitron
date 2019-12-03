e3b8656721d36520c909304a82e01777aa5f73ec
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-12-03 22:57:01 +0900

    fix definition

--- src/main/kotlin/io/github/durun/nitron/core/ast/node/NormalAstRuleNode.kt
+++ src/main/kotlin/io/github/durun/nitron/core/ast/node/NormalAstRuleNode.kt
@@ -2,16 +2,24 @@ package io.github.durun.nitron.core.ast.node
 
 /**
  * 部分木の情報を除くことで抽象化された非終端ノード.
- *
- * @param [originalNode] 元の非終端ノード
  */
 class NormalAstRuleNode(
-        private val originalNode: AstRuleNode,
+        override val ruleName: String,
+        override val range: TextRange?,
         private val text: String? = null
 ) : AstRuleNode {
-    override val ruleName: String = originalNode.ruleName
-    override val children: List<AstNode>? = null
-    override val range: TextRange? = originalNode.range
+    /**
+     *  @param [originalNode] 元の非終端ノード
+     */
+    constructor(originalNode: AstRuleNode, text: String? = null): this(
+            ruleName = originalNode.ruleName,
+            range = originalNode.range,
+            text = text
+    )
+
+    override val children: List<AstNode>?
+        get() = null
+
     override fun getText(): String = text ?: ruleName.toUpperCase()
 }
 
