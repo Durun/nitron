255f1ec3c66ad3f8da40435075466ace322da60e
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-12-07 01:10:59 +0900

    improve: AstFlattenVisitor returns sequence

--- src/main/kotlin/io/github/durun/nitron/core/ast/visitor/AstFlattenVisitor.kt
+++ src/main/kotlin/io/github/durun/nitron/core/ast/visitor/AstFlattenVisitor.kt
@@ -4,18 +4,18 @@ import io.github.durun.nitron.core.ast.node.AstNode
 import io.github.durun.nitron.core.ast.node.AstRuleNode
 import io.github.durun.nitron.core.ast.node.AstTerminalNode
 
-object AstFlattenVisitor : AstVisitor<List<AstTerminalNode>> {
-    override fun visit(node: AstNode): List<AstTerminalNode> {
-        return node.children?.flatMap {
+object AstFlattenVisitor : AstVisitor<Sequence<AstTerminalNode>> {
+    override fun visit(node: AstNode): Sequence<AstTerminalNode> {
+        return node.children?.asSequence()?.flatMap {
             it.accept(this)
-        } ?: emptyList()
+        } ?: emptySequence()
     }
 
-    override fun visitRule(node: AstRuleNode): List<AstTerminalNode> {
+    override fun visitRule(node: AstRuleNode): Sequence<AstTerminalNode> {
         return visit(node)
     }
 
-    override fun visitTerminal(node: AstTerminalNode): List<AstTerminalNode> {
-        return listOf(node)
+    override fun visitTerminal(node: AstTerminalNode): Sequence<AstTerminalNode> {
+        return sequenceOf(node)
     }
 }
