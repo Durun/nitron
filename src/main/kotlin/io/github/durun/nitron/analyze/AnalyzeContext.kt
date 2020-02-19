72260b4ce6070d9477a1a4f53ee812ae9eac7738
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2020-02-19 18:34:54 +0900

    fix AnalyzeContext

--- src/main/kotlin/io/github/durun/nitron/analyze/AnalyzeContext.kt
+++ src/main/kotlin/io/github/durun/nitron/analyze/AnalyzeContext.kt
@@ -6,22 +6,15 @@ import io.github.durun.nitron.core.ast.node.NodeTypePool
 
 data class AnalyzeContext(
         val types: NodeTypePool
-)
+) {
+    fun <T : Any> scope(block: AnalyzeContext.() -> T): T {
+        return this.block()
+    }
 
-
-interface AnalyzeQuery<R : PatternInfo> {
-    fun analyze(pattern: Pattern): R
-}
-
-
-abstract class AnalyzeQueryBase<R : PatternInfo> private constructor(
-        private val types: NodeTypePool
-) : AnalyzeQuery<R> {
-    constructor(context: AnalyzeContext) : this(context.types)
-
-    protected fun type(name: String): NodeType {
+    internal fun typeOf(name: String): NodeType {
         return types.getType(name)
                 ?: throw NoSuchElementException("Wrong declaration of ${this.javaClass}. No such NodeType: $name")
     }
-}
 
+    internal fun typesOf(vararg name: String): List<NodeType> = name.map { typeOf(it) }
+}
