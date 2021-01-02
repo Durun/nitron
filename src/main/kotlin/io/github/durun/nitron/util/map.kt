e3058ba0b01d14fc30d6f355e304dc3c832d3765
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-01-02 17:28:10 +0900

    add util functions

---
+++ src/main/kotlin/io/github/durun/nitron/util/map.kt
@@ -0,0 +1,16 @@
+package io.github.durun.nitron.util
+
+public inline fun <reified K, reified V> Map<*, *>.filterIsInstance(): Map<K, V> {
+	val newMap = mutableMapOf<K, V>()
+	this.entries
+			.filterIsInstance<Map.Entry<K, V>>()
+			.forEach { (key, value) ->
+				newMap[key] = value
+			}
+	return newMap
+}
+
+public fun <V> Map<Int, V>.toSparseList(): List<V?> {
+	val maxIndex = this.keys.maxOrNull() ?: return emptyList()
+	return (0..maxIndex).map { this[it] }
+}
