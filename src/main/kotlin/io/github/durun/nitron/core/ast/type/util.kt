abe16ee052a656018f86924933e1c495c576394e
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-01-05 16:12:59 +0900

    fix detecting synonyms

--- src/main/kotlin/io/github/durun/nitron/core/ast/type/util.kt
+++ src/main/kotlin/io/github/durun/nitron/core/ast/type/util.kt
@@ -4,21 +4,21 @@ import io.github.durun.nitron.core.parser.AstBuildVisitor
 import org.antlr.v4.runtime.Parser
 
 internal fun AstBuildVisitor.nodeTypePoolOf(antlrParser: Parser): NodeTypePool {
-    val tokens = antlrParser.tokenTypeMap.entries
-            .groupBy { it.value }
-            .mapValues { (_, entries) -> entries.map { it.key } }
-            .mapValues { (_, synonyms) ->
-                synonyms.filterNot { it.contains('\'') }
+    val allTokens = antlrParser.tokenTypeMap.entries
+            .map { (name, index) -> TokenType(index, name) }
+    val tokens = allTokens
+            .groupBy { it.index }
+            .map { (_, candidates) ->
+                candidates.filterNot { it.name.contains('\'') }
                         .firstOrNull()
-                        ?: synonyms.first()
+                        ?: candidates.first()
             }
-            .map { (index, name) -> TokenType(index, name) }
     val rules = antlrParser.ruleNames.asList()
             .mapIndexed { index, name -> RuleType(index, name) }
     return NodeTypePool.of(
             tokenTypes = tokens,
             ruleTypes = rules,
-            synonymTokenTypes = tokens.filter { it.index < 0 }
+            synonymTokenTypes = allTokens.filterNot { tokens.any { other -> it === other } }
     )
 }
 
