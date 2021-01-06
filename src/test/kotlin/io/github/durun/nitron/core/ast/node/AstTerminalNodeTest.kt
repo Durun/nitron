e61b2461f747f1b3d12ec79003cae06258f09e15
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-01-06 12:26:09 +0900

    refactor tests

--- src/test/kotlin/io/github/durun/nitron/core/ast/node/AstTerminalNodeTest.kt
+++ src/test/kotlin/io/github/durun/nitron/core/ast/node/AstTerminalNodeTest.kt
@@ -3,10 +3,10 @@ package io.github.durun.nitron.core.ast.node
 import io.github.durun.nitron.core.ast.type.NodeTypePool
 import io.github.durun.nitron.core.ast.type.TokenType
 import io.github.durun.nitron.core.ast.type.TokenTypeSerializer
-import io.github.durun.nitron.core.ast.type.createNodeTypePool
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeSameInstanceAs
+import io.kotest.matchers.types.shouldNotBeSameInstanceAs
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -14,42 +14,43 @@ import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.contextual
 
 class AstTerminalNodeTest : FreeSpec({
-	val node = AstTerminalNode("text", TokenType(1, "Type"), 2)
-
-	"children" {
-		node.children shouldBe null
+	"property" - {
+		val type = TokenType(1, "Type")
+		val node = AstTerminalNode("text", type, 2)
+		"children" {
+			node.children shouldBe null
+		}
+		"getText" {
+			node.getText() shouldBe "text"
+		}
+		"type" {
+			node.type shouldBeSameInstanceAs type
+		}
+		"line" {
+			node.line shouldBe 2
+		}
 	}
 
-	"getText" {
-		node.getText() shouldBe "text"
-	}
-
-	"type" {
-		node.type.name shouldBe "Type"
-		node.type.index shouldBe 1
-	}
-
-	"line" {
-		node.line shouldBe 2
-	}
-
-	"serialize" {
+	"serialize" - {
 		val types = NodeTypePool.of("grammar",
 				tokenTypes = listOf(TokenType(1, "Type")),
 				ruleTypes = listOf()
 		)
-		val node1 = AstTerminalNode("text", types.getTokenType(1)!!, 3)
-
 		val format = Json {
 			serializersModule = SerializersModule {
 				contextual(TokenTypeSerializer(types))
 			}
 		}
+		
+		"serialize" {
+			val node = AstTerminalNode("text", types.getTokenType(1)!!, 3)
 
-		val json = format.encodeToString(node1)
+			val json = format.encodeToString(node)
 
-		val node2: AstTerminalNode = format.decodeFromString(json)
-		node2 shouldBe node1
-		node2.type shouldBeSameInstanceAs node1.type
+			val node2: AstTerminalNode = format.decodeFromString(json)
+			node2 shouldNotBeSameInstanceAs node
+			node2 shouldBe node
+			node2.type shouldBeSameInstanceAs node.type
+		}
 	}
 })
--- src/test/kotlin/io/github/durun/nitron/core/ast/node/NormalAstRuleNodeTest.kt
+++ src/test/kotlin/io/github/durun/nitron/core/ast/node/NormalAstRuleNodeTest.kt
@@ -4,6 +4,7 @@ import io.github.durun.nitron.core.ast.type.*
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeSameInstanceAs
+import io.kotest.matchers.types.shouldNotBeSameInstanceAs
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -25,23 +26,26 @@ class NormalAstRuleNodeTest : FreeSpec({
 		}
 	}
 
-	"serialize" {
+	"serialize" - {
 		val types = NodeTypePool.of("grammar",
 				tokenTypes = listOf(TokenType(1, "Token")),
 				ruleTypes = listOf(RuleType(2, "Rule"))
 		)
-		val node = NormalAstRuleNode(types.getRuleType(2)!!, "text")
-
 		val format = Json {
 			serializersModule = SerializersModule {
 				contextual(RuleTypeSerializer(types))
 			}
 		}
 
-		val json = format.encodeToString(node)
+		"serialize" {
+			val node = NormalAstRuleNode(types.getRuleType(2)!!, "text")
 
-		val node2: NormalAstRuleNode = format.decodeFromString(json)
-		node2 shouldBe node
-		node2.type shouldBeSameInstanceAs node.type
+			val json = format.encodeToString(node)
+
+			val node2: NormalAstRuleNode = format.decodeFromString(json)
+			node2 shouldNotBeSameInstanceAs node
+			node2 shouldBe node
+			node2.type shouldBeSameInstanceAs node.type
+		}
 	}
 })
