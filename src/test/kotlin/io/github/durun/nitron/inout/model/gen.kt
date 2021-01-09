2ed9d7b71a8f41ade621786979a38f12095c7de3
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-01-10 02:29:49 +0900

    fix generator changeType

--- src/test/kotlin/io/github/durun/nitron/inout/model/gen.kt
+++ src/test/kotlin/io/github/durun/nitron/inout/model/gen.kt
@@ -14,15 +14,22 @@ fun Arb.Companion.code(software: String? = null): Arb<Code> = arbitrary { rs ->
 }
 
 fun Arb.Companion.change(software: String? = null): Arb<Change> = arbitrary { rs ->
+	val software = software ?: string(1..5).single(rs)
+	val changeType = enum<ChangeType>().single(rs)
+	val code = when(changeType) {
+		ChangeType.CHANGE -> pair(code(software), code(software)).single(rs)
+		ChangeType.ADD -> null to code(software).single(rs)
+		ChangeType.DELETE -> code(software).single(rs) to null
+	}
 	Change(
-			softwareName = software ?: string(1..5).single(rs),
+			softwareName = software,
 			filePath = file().map(File::toPath).single(rs),
 			author = string(3..10).single(rs),
-			beforeCode = code(software).single(rs),
-			afterCode = code(software).single(rs),
+			beforeCode = code.first,
+			afterCode = code.second,
 			commitHash = string(40).single(rs),
 			date = localDateTime().single(rs),
-			changeType = enum<ChangeType>().single(rs),
+			changeType = changeType,
 			diffType = enum<DiffType>().single(rs)
 	)
 }
