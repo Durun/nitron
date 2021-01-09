d4108b0c0f4fe4c946bdef82f21142c16f8970e7
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-01-09 20:09:33 +0900

    remove unused constructor

--- src/main/kotlin/io/github/durun/nitron/inout/model/ast/structure/simple/AstTableWriter.kt
+++ src/main/kotlin/io/github/durun/nitron/inout/model/ast/structure/simple/AstTableWriter.kt
@@ -1,14 +1,12 @@
 package io.github.durun.nitron.inout.model.ast.structure.simple
 
 import io.github.durun.nitron.core.ast.type.NodeTypePool
-import io.github.durun.nitron.inout.database.SQLiteDatabase
 import io.github.durun.nitron.inout.model.ast.table.NodeTypePools
 import io.github.durun.nitron.inout.model.ast.table.Structures
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.nio.file.Path
 
 const val defaultBufferSize = 10000
 
@@ -18,11 +16,6 @@ class AstTableWriter(
         private val db: Database,
         private val bufferSize: Int = defaultBufferSize
 ) {
-    constructor(path: Path, bufferSize: Int = defaultBufferSize) : this(
-            db = SQLiteDatabase.connect(path),
-            bufferSize = bufferSize
-    )
-
     init {
         transaction(db) {
             SchemaUtils.createMissingTablesAndColumns(NodeTypePools, Structures)
