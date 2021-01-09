15368deccddcb27d354d5ac9845199632f8a8ba9
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-01-09 16:09:46 +0900

    refactoring

--- src/main/kotlin/io/github/durun/nitron/app/AstJsonImportCommand.kt
+++ src/main/kotlin/io/github/durun/nitron/app/AstJsonImportCommand.kt
@@ -4,15 +4,17 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.types.file
 import io.github.durun.nitron.core.ast.type.AstSerializers
-import io.github.durun.nitron.inout.model.ast.Structure
-import io.github.durun.nitron.inout.model.ast.structure.simple.AstJsonReader
+import io.github.durun.nitron.core.ast.type.HashSerializer
+import io.github.durun.nitron.core.ast.type.NodeTypePool
+import io.github.durun.nitron.inout.database.SQLiteDatabase
 import io.github.durun.nitron.inout.model.ast.structure.simple.AstTableWriter
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.jsonObject
+import org.jetbrains.exposed.sql.Database
+import java.io.BufferedReader
 import java.io.File
-import java.nio.file.Path
+import kotlin.io.path.ExperimentalPathApi
 
 class AstJsonImportCommand : CliktCommand(
         name = "importAst"
@@ -23,34 +25,38 @@ class AstJsonImportCommand : CliktCommand(
     private val output: File by argument(name = "output", help = "output Database")
             .file(writable = true)
 
+    @ExperimentalPathApi
     override fun run() {
-        AstJsonImporter(input = input, output = output)
-                .run()
+        SQLiteDatabase.connect(output.toPath()).writeAstJson(
+                input = input.bufferedReader(),
+                fileSize = output.length()
+        )
     }
 }
 
-
-private class AstJsonImporter(
-        private val reader: AstJsonReader,
-        private val writer: AstTableWriter
+fun Database.writeAstJson(
+        input: BufferedReader,
+        fileSize: Long? = null
 ) {
-    constructor(input: Path, output: Path) : this(
-            reader = AstJsonReader(input, bufferSize = 100000, progressOutput = System.out),
-            writer = AstTableWriter(output, bufferSize = 1000)
-    )
+    val typeSet: NodeTypePool = Json.decodeFromString(input.readLine())
+    val decoder = AstSerializers.json(typeSet)
 
-    constructor(input: File, output: File) : this(input.toPath(), output.toPath())
-
-    fun run() {
-        val typeSet = reader.nodeTypePool
-        val decoder = AstSerializers.json(typeSet)
-        val nodes = reader.readLine().map {
-            val obj = Json.parseToJsonElement(it).jsonObject
-            val hash: ByteArray = decoder.decodeFromJsonElement(obj["hash"]!!)
-            val asts = obj["asts"]!!.toString()
-            hash to asts
-        }
-
-        writer.writeBuffering(typeSet, nodes)
-    }
+    var readSize: Long = 0
+    val nodes = input.lineSequence()
+            .onEach {
+                if (fileSize != null) {
+                    // print readSize/fileSize percentage
+                    val before = 100 * readSize / fileSize
+                    readSize += it.toByteArray().size
+                    val after = 100 * readSize / fileSize
+                    if (before != after) print("Progress: $after%\r")
+                }
+            }
+            .map {
+                val obj = Json.parseToJsonElement(it).jsonObject
+                val hash: ByteArray = decoder.decodeFromJsonElement(HashSerializer, obj["hash"]!!)
+                val asts = obj["asts"]!!.toString()
+                hash to asts
+            }
+    AstTableWriter(this).writeBuffering(typeSet, nodes)
 }
