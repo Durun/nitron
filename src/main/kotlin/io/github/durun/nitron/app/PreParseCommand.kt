eb341d5fef481fda2cf1368bc357a1331c66ff00
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-03-06 13:20:10 +0900

    add validation remotes

--- src/main/kotlin/io/github/durun/nitron/app/PreParseCommand.kt
+++ src/main/kotlin/io/github/durun/nitron/app/PreParseCommand.kt
@@ -2,14 +2,12 @@ package io.github.durun.nitron.app
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.options.convert
-import com.github.ajalt.clikt.parameters.options.multiple
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.validate
+import com.github.ajalt.clikt.parameters.options.*
 import com.github.ajalt.clikt.parameters.types.path
 import io.github.durun.nitron.inout.database.SQLiteDatabase
 import io.github.durun.nitron.inout.model.preparse.RepositoryTable
 import io.github.durun.nitron.util.logger
+import org.eclipse.jgit.api.Git
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.insertIgnoreAndGetId
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -22,9 +20,7 @@ class PreParseCommand : CliktCommand(name = "preparse") {
 	private val urls: List<URL> by option("--url", help = "Git repository URL")
 		.convert { URL(it) }
 		.multiple()
-		.validate {
-			// TODO: Validate URL
-		}
+		.validate(gitUrlValidator)
 
 	private val log by logger()
 
@@ -43,5 +39,20 @@ class PreParseCommand : CliktCommand(name = "preparse") {
 			}
 		}
 		log.debug { "Closed $dbFile" }
+	}
+}
+
+private val gitUrlValidator: OptionValidator<List<URL>> = { urls ->
+	val results = urls.map { url ->
+		runCatching {
+			val refs = Git.lsRemoteRepository()
+				.setRemote(url.toString())
+				.call()
+			refs.size
+		}
+	}
+	val errors = results.mapNotNull { it.exceptionOrNull() }
+	require(errors.isEmpty()) {
+		"Invalid remotes:\n${errors.joinToString("\n") { it.localizedMessage }.prependIndent("\t")}"
 	}
 }
