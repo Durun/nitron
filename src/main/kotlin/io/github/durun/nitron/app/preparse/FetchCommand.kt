acdd7535f62d0fc61b866286041aadae9c8e8933
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-03-06 18:51:00 +0900

    add process detecting file changes

--- src/main/kotlin/io/github/durun/nitron/app/preparse/FetchCommand.kt
+++ src/main/kotlin/io/github/durun/nitron/app/preparse/FetchCommand.kt
@@ -12,7 +12,14 @@ import io.github.durun.nitron.inout.model.preparse.RepositoryTable
 import io.github.durun.nitron.util.logger
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.api.ListBranchCommand
+import org.eclipse.jgit.diff.DiffFormatter
+import org.eclipse.jgit.diff.RawTextComparator
+import org.eclipse.jgit.lib.Constants
 import org.eclipse.jgit.lib.Ref
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.treewalk.TreeWalk
+import org.eclipse.jgit.util.io.DisabledOutputStream
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.File
@@ -44,9 +51,15 @@ class FetchCommand : CliktCommand(name = "preparse-fetch") {
 		}
 		log.info { "Fetch list:\n${repos.joinToString("\n").prependIndent("\t")}" }
 		repos.forEach { repoUrl ->
-			val repo = openRepository(repoUrl)
-			val mainBranch = detectMainBranch(repo)
+			val git = openRepository(repoUrl)
+			val mainBranch = detectMainBranch(git)
 			log.info { "Detected main branch is: $mainBranch" }
+			git.checkout()
+				.setName(mainBranch.name)
+				.call()
+			log.info { "Checked out: $mainBranch" }
+			getCommitSequence(git)
+				.forEach { TODO() }
 		}
 	}
 
@@ -65,13 +78,91 @@ class FetchCommand : CliktCommand(name = "preparse-fetch") {
 	}
 
 	private fun detectMainBranch(repo: Git): Ref {
+		log.info { "Detecting main" }
 		val branches: List<Ref> = repo.branchList()
 			.setListMode(ListBranchCommand.ListMode.REMOTE)
-			.setContains("main")
-			.setContains("master")
 			.call()
 		return branches.firstOrNull()
 			?: throw Exception("No main branch")
 	}
+
 }
 
+data class CommitInfo(
+	val id: String,
+	val message: String,
+	val files: Collection<FileInfo>
+)
+
+data class FileInfo(
+	val path: String,
+	val readText: () -> String
+)
+
+
+private fun getCommitSequence(git: Git): Sequence<CommitInfo> {
+	val commitPairs = git.log()
+		.all()
+		.call()
+		.zipWithNext()
+	require(commitPairs.isNotEmpty()) { "Commits are less than 2" }
+	val initialCommit = commitPairs.last().second
+	return commitPairs.asSequence().map { (commit, parent) ->
+		CommitInfo(
+			id = commit.id.name,
+			message = commit.fullMessage,
+			files = detectCommitInfo(git.repository, commit, parent) { true }
+		)
+	} + sequence {
+		CommitInfo(
+			id = initialCommit.id.name,
+			message = initialCommit.fullMessage,
+			files = detectCommitInfo(git.repository, initialCommit, parent = null) { true }
+		)
+	}
+}
+
+private fun detectCommitInfo(
+	repository: Repository,
+	commit: RevCommit,
+	parent: RevCommit?,
+	filter: (fileName: String) -> Boolean
+): Collection<FileInfo> {
+	if (parent != null) {
+		val entries = DiffFormatter(DisabledOutputStream.INSTANCE)
+			.let {
+				it.setRepository(repository)
+				it.setDiffComparator(RawTextComparator.DEFAULT)
+				it.isDetectRenames = true
+				val entries = it.scan(parent, commit)
+				it.close()
+				entries
+			}
+		val reader = repository.newObjectReader()
+		return entries
+			.filter { filter(it.newPath) }
+			.map {
+				FileInfo(path = it.newPath) {
+					reader.open(it.newId.toObjectId(), Constants.OBJ_BLOB)
+						.cachedBytes
+						.decodeToString()
+				}
+			}
+	} else {
+		// if commit is the initial commit
+		val treewalk = TreeWalk(repository)
+			.apply {
+				addTree(commit.tree)
+				isRecursive = true
+			}
+		return mutableListOf<FileInfo>().apply {
+			while (treewalk.next()) {
+				val info = FileInfo(treewalk.pathString) {
+					repository.open(treewalk.getObjectId(0))
+						.cachedBytes.decodeToString()
+				}
+				add(info)
+			}
+		}
+	}
+}
