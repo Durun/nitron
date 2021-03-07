24ee527d7b43c5243503277a9178a4727d3a86ae
Durun <NegiIgaiNuki@gmail.com>
2021-03-07 20:42:51 +0900

    add inflateAndDeflateText

--- src/test/kotlin/io/github/durun/nitron/app/preparse/ParseUtilTest.kt
+++ src/test/kotlin/io/github/durun/nitron/app/preparse/ParseUtilTest.kt
@@ -1,5 +1,7 @@
 package io.github.durun.nitron.app.preparse
 
+import io.github.durun.nitron.core.toBlob
+import io.github.durun.nitron.core.toByteArray
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
@@ -8,15 +10,28 @@ import io.kotest.property.arbitrary.take
 
 class ParseUtilTest : FreeSpec({
 
-    "inflateAndDeflateText" {
-        Arb.string().take(10).forEach { input->
-            println(input)
+    "inflateAndDeflateText" - {
+        "text" {
+            Arb.string().take(10).forEach { input ->
+                println(input)
 
-            val bytes = input.toByteArray().deflate()
-            val output = bytes.inflate().decodeToString()
+                val bytes = input.toByteArray().deflate()
+                val output = bytes.inflate().decodeToString()
 
-            println(output)
-            output shouldBe input
+                println(output)
+                output shouldBe input
+            }
+        }
+        "over blob" {
+            Arb.string().take(10).forEach { input ->
+                println(input)
+
+                val bytes = input.toByteArray().deflate().toBlob()
+                val output = bytes.toByteArray().inflate().decodeToString()
+
+                println(output)
+                output shouldBe input
+            }
         }
     }
 })
