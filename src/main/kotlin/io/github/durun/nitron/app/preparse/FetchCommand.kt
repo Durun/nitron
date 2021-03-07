a648a3629a6f3219508d4f0ec1637dfe9b3dd643
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-03-07 09:54:43 +0900

    small fix and reformat

--- src/main/kotlin/io/github/durun/nitron/app/preparse/FetchCommand.kt
+++ src/main/kotlin/io/github/durun/nitron/app/preparse/FetchCommand.kt
@@ -8,36 +8,12 @@ import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
 import com.github.ajalt.clikt.parameters.types.int
 import com.github.ajalt.clikt.parameters.types.path
-import io.github.durun.nitron.core.MD5
 import io.github.durun.nitron.core.config.loader.NitronConfigLoader
 import io.github.durun.nitron.inout.database.SQLiteDatabase
-import io.github.durun.nitron.inout.model.preparse.CommitTable
-import io.github.durun.nitron.inout.model.preparse.FileTable
-import io.github.durun.nitron.inout.model.preparse.RepositoryTable
 import io.github.durun.nitron.util.logger
 import kotlinx.coroutines.*
-import org.eclipse.jgit.api.Git
-import org.eclipse.jgit.api.ListBranchCommand
-import org.eclipse.jgit.diff.DiffFormatter
-import org.eclipse.jgit.diff.RawTextComparator
-import org.eclipse.jgit.lib.Constants
-import org.eclipse.jgit.lib.ObjectId
-import org.eclipse.jgit.lib.Ref
-import org.eclipse.jgit.lib.Repository
-import org.eclipse.jgit.revwalk.RevCommit
-import org.eclipse.jgit.treewalk.TreeWalk
-import org.eclipse.jgit.util.io.DisabledOutputStream
-import org.jetbrains.exposed.dao.EntityID
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.transactions.experimental.suspendedTransactionAsync
-import org.jetbrains.exposed.sql.transactions.transaction
-import org.joda.time.DateTime
 import java.io.File
-import java.net.URL
 import java.nio.file.Path
-import java.time.LocalDateTime
-import java.util.Date
-import kotlin.streams.asStream
 
 class FetchCommand : CliktCommand(name = "preparse-fetch") {
 
@@ -51,7 +27,7 @@ class FetchCommand : CliktCommand(name = "preparse-fetch") {
     private val dbFile: Path by argument(name = "DATABASE", help = "Database file")
         .path(writable = true)
 
-    private val bufferSize: Int? by option("-b")
+    private val bufferSize: Int by option("-b")
         .int()
         .default(1000)
 
