dc2eaac0f1f5e1d0cffeee6ca1018734a15e79d7
Durun <NegiIgaiNuki@gmail.com>
2021-03-08 15:51:18 +0900

    refactor

--- src/main/kotlin/io/github/durun/nitron/app/preparse/ParseCommand.kt
+++ src/main/kotlin/io/github/durun/nitron/app/preparse/ParseCommand.kt
@@ -6,18 +6,12 @@ import com.github.ajalt.clikt.parameters.options.defaultLazy
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
 import com.github.ajalt.clikt.parameters.types.path
-import io.github.durun.nitron.core.MD5
 import io.github.durun.nitron.core.config.loader.NitronConfigLoader
 import io.github.durun.nitron.inout.database.SQLiteDatabase
-import io.github.durun.nitron.inout.model.preparse.AstContentTable
-import io.github.durun.nitron.inout.model.preparse.AstTable
-import io.github.durun.nitron.inout.model.preparse.RepositoryTable
+import io.github.durun.nitron.inout.model.preparse.*
 import io.github.durun.nitron.util.logger
-import org.jetbrains.exposed.dao.EntityID
-import org.jetbrains.exposed.sql.insertIgnoreAndGetId
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.jetbrains.exposed.sql.update
 import java.io.File
 import java.net.URL
 import java.nio.file.Path
@@ -58,22 +52,19 @@ class ParseCommand : CliktCommand(name = "preparse") {
                 .last()
         }
 
-        val git = gitUtil.openRepository(URL(repoUrl))
-        val parseUtil = ParseUtil(git, config)
-
-        transaction(db) {
-            val (astId, objectId, lang) = dbUtil.queryAbsentAst(repoId).first()
-
-            val parsed = parseUtil.parseText(parseUtil.readFile(objectId)!!, lang, config.langConfig[lang]!!)
-
-            val contentId = AstContentTable.insertIgnoreAndGetId {
-                it[content] = parsed
-                it[checksum] = MD5.digest(parsed).toString()
-            }
-            AstTable.update({ AstTable.id eq astId }) {
-                it[content] = contentId
-            }
+        // normalize
+        transaction {
+            AstTable.setNullOnAbsentContent()
         }
 
+        // parse
+        val git = gitUtil.openRepository(URL(repoUrl))
+        val parseUtil = ParseUtil(git, config)
+        transaction(db) {
+            val (astId, objectId, lang) = dbUtil.queryAbsentAst(repoId).first()
+            val parsed = parseUtil.parseText(parseUtil.readFile(objectId)!!, lang, config.langConfig[lang]!!)
+            val contentId = AstContentTable.insertAndGetId(parsed)
+            AstTable.updateContent(astId, contentId)
+        }
     }
 }
