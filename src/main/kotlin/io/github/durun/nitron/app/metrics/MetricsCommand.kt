abe8bbfccbbebe15d32601b635968a85ad46864a
Durun <negiigainuki@gmail.com>
2021-04-30 14:44:09 +0900

    MetricsCommand deals with multi DB input

--- src/main/kotlin/io/github/durun/nitron/app/metrics/MetricsCommand.kt
+++ src/main/kotlin/io/github/durun/nitron/app/metrics/MetricsCommand.kt
@@ -3,16 +3,13 @@ package io.github.durun.nitron.app.metrics
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.path
+import io.github.durun.nitron.inout.database.SQLiteDatabase
 import io.github.durun.nitron.util.logger
+import org.jetbrains.exposed.sql.Database
 import java.nio.file.Path
 
 class MetricsCommand : CliktCommand(name = "metrics") {
-    private val outDbFiles: Path by option("-o", help = "Output Database file")
-        .path()
-        .required()
     private val dbFiles: List<Path> by argument(name = "DATABASE", help = "Database file")
         .path(readable = true)
         .multiple()
@@ -20,6 +17,14 @@ class MetricsCommand : CliktCommand(name = "metrics") {
     private val log by logger()
 
     override fun run() {
-        TODO("Not yet implemented")
+        dbFiles.forEach { dbFile ->
+            val db = SQLiteDatabase.connect(dbFile)
+            processOneDb(db)
+            log.info { "Done: $dbFile" }
+        }
+    }
+
+    private fun processOneDb(db: Database) {
+        TODO()
     }
 }
