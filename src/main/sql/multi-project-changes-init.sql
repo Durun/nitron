a0628147539e91d434f29d13c509565a56ce855c
Durun <negiigainuki@gmail.com>
2021-04-30 14:30:53 +0900

    add SQL to merge multi project changes

---
+++ src/main/sql/multi-project-changes-init.sql
@@ -0,0 +1,41 @@
+CREATE TABLE codes
+(
+    hash blob,
+    text string,
+    primary key (hash)
+);
+CREATE TABLE revisions
+(
+    software string,
+    id       string,
+    date     string,
+    message  string,
+    author   string,
+    primary key (software, id)
+);
+CREATE TABLE changes
+(
+    software   string,
+    filepath   string,
+    beforeHash blob,
+    afterHash  blob,
+    revision   string
+);
+CREATE TABLE patterns
+(
+    software   string,
+    beforeHash blob,
+    afterHash  blob,
+    support    integer,
+    confidence real,
+    primary key (software, beforeHash, afterHash)
+);
+
+CREATE
+INDEX index_patterns_beforeHash on patterns(beforeHash);
+CREATE
+INDEX index_patterns_afterHash on patterns(afterHash);
+CREATE
+INDEX index_patterns_beforeHash_afterHash on patterns(beforeHash, afterHash);
+CREATE
+INDEX index_nTexts_hash on codes(hash);
