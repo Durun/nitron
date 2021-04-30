0bf4b5ba68810bba2e11d90f853a7ded44ca86c3
Durun <negiigainuki@gmail.com>
2021-04-30 16:18:17 +0900

    calculate test

--- src/main/kotlin/io/github/durun/nitron/app/metrics/MetricsCommand.kt
+++ src/main/kotlin/io/github/durun/nitron/app/metrics/MetricsCommand.kt
@@ -4,16 +4,16 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
 import com.github.ajalt.clikt.parameters.types.path
+import io.github.durun.nitron.core.MD5
+import io.github.durun.nitron.core.toMD5
 import io.github.durun.nitron.inout.database.SQLiteDatabase
 import io.github.durun.nitron.inout.model.metrics.ChangesTable
 import io.github.durun.nitron.inout.model.metrics.GlobalPatternsTable
 import io.github.durun.nitron.util.logger
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.nio.file.Path
+import java.sql.Blob
 
 class MetricsCommand : CliktCommand(name = "metrics") {
     private val dbFiles: List<Path> by argument(name = "DATABASE", help = "Database file")
@@ -34,20 +34,62 @@ class MetricsCommand : CliktCommand(name = "metrics") {
         transaction(db) {
             SchemaUtils.createMissingTablesAndColumns(GlobalPatternsTable)
         }
-        val patterns = transaction(db) {
-            ChangesTable.slice(ChangesTable.beforeHash, ChangesTable.afterHash)
-                .selectAll().withDistinct()
-                .map { it[ChangesTable.beforeHash] to it[ChangesTable.afterHash] }
+        val changes = transaction(db) {
+            ChangesTable
+                .selectAll()
+                .map {
+                    Change(
+                        software = it[ChangesTable.software],
+                        filePath = it[ChangesTable.filepath],
+                        pattern = Pattern(
+                            hash = it[ChangesTable.beforeHash]?.toMD5() to it[ChangesTable.afterHash]?.toMD5(),
+                            blob = it[ChangesTable.beforeHash] to it[ChangesTable.afterHash]
+                        )
+                    )
+
+                }
         }
         transaction(db) {
-            patterns.forEach { (beforeBlob, afterBlob) ->
+            GlobalPatternsTable.deleteAll()
+        }
+        val patterns = changes.map { it.pattern }.distinct()
+        transaction(db) {
+            patterns.forEachIndexed { i, (hash, blob) ->
+                val sup = changes.count { it.pattern.hash.first == hash.first }
                 GlobalPatternsTable.insert {
-                    it[beforeHash] = beforeBlob
-                    it[afterHash] = afterBlob
-                    it[support] = 0         // TODO
+                    it[beforeHash] = blob.first
+                    it[afterHash] = blob.second
+                    it[support] = sup
                     it[confidence] = 0.0    // TODO
                 }
+                if (i % 1000 == 0) log.info { "Done: $i / ${patterns.size}" }
             }
         }
+    }
+}
+
+private data class Change(
+    val software: String,
+    val filePath: String,
+    val pattern: Pattern
+)
+
+private data class Pattern(
+    val hash: Pair<MD5?, MD5?>,
+    val blob: Pair<Blob?, Blob?>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Pattern
+
+        if (hash != other.hash) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return hash.hashCode()
     }
 }
