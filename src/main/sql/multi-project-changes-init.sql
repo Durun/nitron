790e2d76ad11d4221d118af6c8cb76d67b86813c
Durun <negiigainuki@gmail.com>
2021-04-30 18:19:13 +0900

    simplify SQL

--- src/main/sql/multi-project-changes-init.sql
+++ src/main/sql/multi-project-changes-init.sql
@@ -21,29 +21,6 @@ CREATE TABLE changes
     afterHash  blob,
     revision   string
 );
-CREATE TABLE patterns
-(
-    software   string,
-    beforeHash blob,
-    afterHash  blob,
-    support    integer,
-    confidence real,
-    primary key (software, beforeHash, afterHash)
-);
-CREATE TABLE globalPatterns
-(
-    beforeHash blob,
-    afterHash  blob,
-    support    integer,
-    confidence real,
-    primary key (beforeHash, afterHash)
-);
 
-CREATE
-INDEX index_patterns_beforeHash on patterns(beforeHash);
-CREATE
-INDEX index_patterns_afterHash on patterns(afterHash);
-CREATE
-INDEX index_patterns_beforeHash_afterHash on patterns(beforeHash, afterHash);
 CREATE
 INDEX index_nTexts_hash on codes(hash);
