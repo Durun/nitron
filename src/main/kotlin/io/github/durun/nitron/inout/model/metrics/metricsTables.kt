169f8b65db1b2a243191dd3963840450bec327e4
Durun <negiigainuki@gmail.com>
2021-04-30 14:40:39 +0900

    revise table definition

--- src/main/kotlin/io/github/durun/nitron/inout/model/metrics/metricsTables.kt
+++ src/main/kotlin/io/github/durun/nitron/inout/model/metrics/metricsTables.kt
@@ -1,24 +1,37 @@
 package io.github.durun.nitron.inout.model.metrics
 
-import org.jetbrains.exposed.dao.IntIdTable
 import org.jetbrains.exposed.sql.Column
-import org.joda.time.DateTime
+import org.jetbrains.exposed.sql.Table
 import java.sql.Blob
 
-object PatternMetricsTable : IntIdTable("pattern_metrics") {
-    // id
+object CodesTable : Table("codes") {
+    val hash: Column<Blob> = blob("hash")
+    val text: Column<String> = text("text")
+}
+
+object RevisionsTable : Table("revisions") {
+    val software: Column<String> = text("software")
+    val id: Column<String> = text("id")
+    val date: Column<String> = text("date")
+    val message: Column<String> = text("message")
+    val author: Column<String> = text("author")
+}
+
+object ChangesTable : Table("changes") {
+    val software: Column<String> = text("software")
+    val filepath: Column<String> = text("filepath")
     val beforeHash: Column<Blob?> = blob("beforeHash").nullable()
     val afterHash: Column<Blob?> = blob("afterHash").nullable()
+    val revision: Column<String> = text("revision")
+}
 
-    // content
-    val beforeNText: Column<String?> = text("beforeNText").nullable()
-    val afterNText: Column<String?> = text("afterNText").nullable()
+object PatternsTable : Table("patterns") {
+    // id
+    val software: Column<String> = text("software")
+    val beforeHash: Column<Blob?> = blob("beforeHash").nullable()
+    val afterHash: Column<Blob?> = blob("afterHash").nullable()
 
     // metrics
     val support: Column<Int> = integer("support")
     val confidence: Column<Double> = double("confidence")
-    val authors: Column<Int> = integer("authors")
-    val files: Column<Int> = integer("files")
-    val firstDate: Column<DateTime> = datetime("firstDate")
-    val lastDate: Column<DateTime> = datetime("firstDate")
 }
