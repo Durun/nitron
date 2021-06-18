532f973b71399cf9d07b8428f7b159593291441f
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-06-18 18:01:31 +0900

    use AstBuilder instead of AstBuildVisitor and GenericParser

--- src/main/kotlin/io/github/durun/nitron/app/AstPrintCommand.kt
+++ src/main/kotlin/io/github/durun/nitron/app/AstPrintCommand.kt
@@ -9,8 +9,8 @@ import com.github.ajalt.clikt.parameters.types.file
 import com.github.ajalt.clikt.parameters.types.path
 import io.github.durun.nitron.core.ast.visitor.AstPrintVisitor
 import io.github.durun.nitron.core.config.loader.LangConfigLoader
-import io.github.durun.nitron.core.parser.antlr.AstBuildVisitor
-import io.github.durun.nitron.core.parser.antlr.GenericParser
+import io.github.durun.nitron.core.parser.AstBuilders
+import io.github.durun.nitron.core.parser.antlr.antlr
 import java.io.BufferedWriter
 import java.io.File
 import java.nio.file.Path
@@ -48,14 +48,15 @@ class AstPrintCommand : CliktCommand(
     @ExperimentalPathApi
     override fun run() {
         val config = LangConfigLoader.load(configPath)
-        val parser = GenericParser.fromFiles(
-                grammarFiles = config.grammar.grammarFilePaths,
-                utilityJavaFiles = config.grammar.utilJavaFilePaths
+        val astBuilder = AstBuilders.antlr(
+            grammarName = config.fileName,
+            entryPoint = config.grammar.startRule,
+            grammarFiles = config.grammar.grammarFilePaths,
+            utilityJavaFiles = config.grammar.utilJavaFilePaths
         )
         inputs
             .forEach { input ->
-                val tree = parser.parse(input.bufferedReader(), config.grammar.startRule)
-                val ast = tree.accept(AstBuildVisitor(grammarName = config.fileName, parser = parser.antlrParser))
+                val ast = astBuilder.parse(input.bufferedReader())
                 val text = ast.accept(AstPrintVisitor)
                 output.appendLine("\n@ ${input.path}")
                 output.appendLine(text)
