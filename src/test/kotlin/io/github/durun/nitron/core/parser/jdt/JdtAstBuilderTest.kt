a0117155dce9999972810850f1b3f831b61c9a28
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-06-22 15:59:42 +0900

    update test
    update test

--- src/test/kotlin/io/github/durun/nitron/core/parser/jdt/JdtAstBuilderTest.kt
+++ src/test/kotlin/io/github/durun/nitron/core/parser/jdt/JdtAstBuilderTest.kt
@@ -3,15 +3,22 @@ package io.github.durun.nitron.core.parser.jdt
 import io.github.durun.nitron.core.ast.visitor.AstPrintVisitor
 import io.github.durun.nitron.core.parser.AstBuilders
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class JdtAstBuilderTest : FreeSpec({
 
     val src = """
         package example;
-        import java.util.*;
-        public class HelloWorld { 
+        import java.util.List;
+        interface I {
+          void greet();
+        }
+        private class C implements I, I2 {
+          @Override
+          void greet() { System.out.println("Hello, World"); }
+        }
+        public class HelloWorld {
            public static void main(String[] args) { 
-              System.out.println("Hello, World");
               y = (x*x) + 1;
            }
         }
@@ -22,5 +29,8 @@ class JdtAstBuilderTest : FreeSpec({
         val ast = parser.parse(src.reader())
         println(ast)
         println(ast.accept(AstPrintVisitor))
+        ast.toString().removeSpaceAndNL() shouldBe src.removeSpaceAndNL()
     }
 })
+
+private fun String.removeSpaceAndNL() = this.filterNot { it in " \n" }
