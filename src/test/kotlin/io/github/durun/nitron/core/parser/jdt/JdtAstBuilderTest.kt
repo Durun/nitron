efcadb342b52dce5cd5321ed27ce14e6e086ab47
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-06-23 22:07:40 +0900

    update test

--- src/test/kotlin/io/github/durun/nitron/core/parser/jdt/JdtAstBuilderTest.kt
+++ src/test/kotlin/io/github/durun/nitron/core/parser/jdt/JdtAstBuilderTest.kt
@@ -1,8 +1,12 @@
 package io.github.durun.nitron.core.parser.jdt
 
+import io.github.durun.nitron.core.ast.node.AstTerminalNode
 import io.github.durun.nitron.core.ast.visitor.AstPrintVisitor
+import io.github.durun.nitron.core.ast.visitor.flatten
 import io.github.durun.nitron.core.parser.AstBuilders
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 
 class JdtAstBuilderTest : FreeSpec({
@@ -15,7 +19,9 @@ class JdtAstBuilderTest : FreeSpec({
         }
         private class C implements I, I2 {
           @Override
-          void greet() { System.out.println("Hello, World"); }
+          void greet() { 
+            System.out.println("Hello, World"); 
+          }
         }
         public class HelloWorld {
            public static void main(String[] args) { 
@@ -30,6 +36,21 @@ class JdtAstBuilderTest : FreeSpec({
         println(ast)
         println(ast.accept(AstPrintVisitor))
         ast.toString().removeSpaceAndNL() shouldBe src.removeSpaceAndNL()
+
+        ast.flatten().forAll { token ->
+            token should { it is AstTerminalNode }
+        }
+
+        val tokens = ast.flatten().filterIsInstance<AstTerminalNode>()
+        tokens.forEach {
+            println("${it.line}: $it")
+        }
+        val src2 = tokens.groupBy { it.line }
+            .entries.sortedBy { (line, _) -> line }
+            .joinToString("\n") { (_, tokens) -> tokens.joinToString("") }
+        println(src)
+        println(src2)
+        src2.filter { it != ' ' } shouldBe src.filter { it != ' ' }
     }
 })
 
