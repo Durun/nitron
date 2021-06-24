02aa7052d93d6155c39af2ba267703aa125cc703
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-06-24 19:01:54 +0900

    add speed test

---
+++ src/test/kotlin/io/github/durun/nitron/validation/javaSpeedTest.kt
@@ -0,0 +1,42 @@
+package io.github.durun.nitron.validation
+
+import io.github.durun.nitron.core.ast.visitor.AstPrintVisitor
+import io.github.durun.nitron.core.config.loader.NitronConfigLoader
+import io.github.durun.nitron.core.parser.AstBuilders
+import io.github.durun.nitron.core.parser.antlr.antlr
+import io.github.durun.nitron.core.parser.jdt.jdt
+import java.nio.file.Paths
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+
+private val configPath = Paths.get("config/nitron.json")
+
+/**
+ * @param args filepath to parse (multiple OK)
+ */
+@ExperimentalTime
+fun main(args: Array<String>) = TemporaryTest {
+    val sources = args.map { Paths.get(it).toFile() }
+    "JDT Parser" {
+        val parser = AstBuilders.jdt()
+        sources.forEach { src ->
+            val time = measureTime {
+                parser.parse(src.reader())
+                    .also { println(it.accept(AstPrintVisitor)) }
+            }
+            println("time JDT  : $time")
+        }
+    }
+    "ANTLR" {
+        val config = NitronConfigLoader.load(configPath).langConfig["java"]?.grammar ?: throw Exception()
+        val parser =
+            AstBuilders.antlr(config.fileName, config.startRule, config.grammarFilePaths, config.utilJavaFilePaths)
+        sources.forEach { src ->
+            val time = measureTime {
+                parser.parse(src.reader())
+                    .also { println(it.accept(AstPrintVisitor)) }
+            }
+            println("time ANTLR: $time")
+        }
+    }
+}.execute()
