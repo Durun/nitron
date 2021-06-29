44d0ef61106f1c61f40af0ee086b12590f4628f5
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-06-30 00:41:44 +0900

    refactor test code

--- src/test/kotlin/io/github/durun/nitron/core/parser/antlr/ParserStore.kt
+++ src/test/kotlin/io/github/durun/nitron/core/parser/antlr/ParserStore.kt
@@ -6,26 +6,19 @@ import io.kotest.mpp.log
 import java.nio.file.Path
 
 object ParserStore {
-    private val parsers = mutableMapOf<Pair<Set<Path>, Set<Path>?>, Result<GenericParser>>()
+    private val parsers = mutableMapOf<Pair<Set<Path>, Set<Path>>, GenericParser>()
 
-    private val mapping: (Pair<Set<Path>, Set<Path>>) -> Result<GenericParser> = { (grammarFiles, utilityJavaFiles) ->
-        runCatching {
-            val p = GenericParser.fromFiles(grammarFiles, utilityJavaFiles)
-            log { "Compiled grammar: ${p.antlrParser.grammarFileName}" }
-            p
-        }
-    }
-
+    fun getOrNull(config: ParserConfig): GenericParser? = runCatching { getOrThrow(config) }.getOrNull()
     fun getOrThrow(config: ParserConfig): GenericParser = getOrThrow(config as AntlrParserConfig)
     fun getOrThrow(config: AntlrParserConfig): GenericParser =
         getOrThrow(config.grammarFilePaths, config.utilJavaFilePaths)
-
     fun getOrThrow(grammarFiles: Collection<Path>, utilJavaFiles: Collection<Path> = emptySet()): GenericParser {
-        val key = grammarFiles.toSet() to utilJavaFiles.toSet()
         return synchronized(parsers) {
-            parsers.computeIfAbsent(key) { mapping(key) }
-        }.getOrThrow()
+            parsers.computeIfAbsent(grammarFiles.toSet() to utilJavaFiles.toSet()) { (gFiles, uFiles) ->
+                val parser = GenericParser.fromFiles(gFiles, uFiles)
+                log { "Compiled grammar: ${parser.antlrParser.grammarFileName}" }
+                parser
+            }
+        }
     }
-
-    fun getOrNull(config: ParserConfig): GenericParser? = runCatching { getOrThrow(config) }.getOrNull()
 }
