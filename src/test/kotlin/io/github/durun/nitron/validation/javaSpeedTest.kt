4748d569534a47b030f9ca04c0d19862e363fbc1
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-06-29 20:43:13 +0900

    fix

--- src/test/kotlin/io/github/durun/nitron/validation/javaSpeedTest.kt
+++ src/test/kotlin/io/github/durun/nitron/validation/javaSpeedTest.kt
@@ -3,7 +3,6 @@ package io.github.durun.nitron.validation
 import io.github.durun.nitron.core.ast.visitor.AstPrintVisitor
 import io.github.durun.nitron.core.config.loader.NitronConfigLoader
 import io.github.durun.nitron.core.parser.AstBuilders
-import io.github.durun.nitron.core.parser.antlr.antlr
 import io.github.durun.nitron.core.parser.jdt.jdt
 import java.nio.file.Paths
 import kotlin.time.ExperimentalTime
@@ -28,9 +27,8 @@ fun main(args: Array<String>) = TemporaryTest {
         }
     }
     "ANTLR" {
-        val config = NitronConfigLoader.load(configPath).langConfig["java"]?.grammar ?: throw Exception()
-        val parser =
-            AstBuilders.antlr(config.fileName, config.startRule, config.grammarFilePaths, config.utilJavaFilePaths)
+        val config = NitronConfigLoader.load(configPath).langConfig["java"]?.parserConfig ?: throw Exception()
+        val parser = config.getParser()
         sources.forEach { src ->
             val time = measureTime {
                 parser.parse(src.reader())
