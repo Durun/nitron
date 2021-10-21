6e17c0a73bf4403cba00ae3ee7b45ee85fdf6ba3
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2021-10-21 19:09:27 +0900

    add Throws annotation to NitronParser

--- src/main/kotlin/com/github/durun/nitron/core/parser/NitronParser.kt
+++ src/main/kotlin/com/github/durun/nitron/core/parser/NitronParser.kt
@@ -1,11 +1,14 @@
 package com.github.durun.nitron.core.parser
 
+import com.github.durun.nitron.core.ParsingException
 import com.github.durun.nitron.core.ast.node.AstNode
 import com.github.durun.nitron.core.ast.type.NodeTypePool
 import java.io.Reader
 
 interface NitronParser {
     val nodeTypes: NodeTypePool
+
+    @Throws(ParsingException::class)
     fun parse(reader: Reader): AstNode
 }
 
